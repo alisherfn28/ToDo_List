c44de2be3e426f7b52992e1c2ffe3ab961f403b2
alisher <alisher@gmail.com>
2024-11-03 21:00:35 +0500

    bitib qoldi

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const button = document.querySelector("#btn");
+const block = document.querySelector("#block");
+const userTask = document.querySelector("#Task");
+
+function createCard(data) {
+  return `
+   <div class="card">
+          <div class="title">
+            <h3>${data.Task}</h3>
+          </div>
+          <div class="images">
+            <img src="./img/pen.svg" alt=" " />
+            <img src="./img/clear.svg" id="img2" alt=" " />
+          </div>
+        </div>
+    `;
+}
+
+button &&
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (userTask.value.trim() !== "") {
+      let data = {
+        Task: userTask.value,
+      };
+
+      let card = createCard(data);
+      block.innerHTML += card;
+
+      userTask.value = "";
+    } else {
+      alert("Iltimos taskizi kiriting !!!");
+      userTask.focus();
+    }
+  });
